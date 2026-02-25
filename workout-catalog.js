cdcdc07e98c40456c78797e0c62ea4c0eaf8b566
spencamp <166778013+spencamp@users.noreply.github.com>
2026-02-24 21:27:21 -0800

    Guard app bootstrap to prevent duplicate script initialization failures

---
+++ workout-catalog.js
@@ -0,0 +1,52 @@
+window.WORKOUT_CATALOG = [
+  {
+    name: "dead hang",
+    type: "exercise",
+    aliases: ["hang", "dead-hang", "bar hang"],
+  },
+  {
+    name: "pull ups",
+    type: "exercise",
+    aliases: ["pull-ups", "pullup", "pull-up", "pullups"],
+  },
+  {
+    name: "push ups",
+    type: "exercise",
+    aliases: ["push-ups", "pushup", "push-up", "pushups"],
+  },
+  {
+    name: "squats",
+    type: "exercise",
+    aliases: ["air squats", "bodyweight squats"],
+  },
+  {
+    name: "deep squat hold",
+    type: "stretch",
+    aliases: ["deep squat", "dep squat", "third world squat"],
+  },
+  {
+    name: "couch stretch",
+    type: "stretch",
+    aliases: ["hip flexor stretch"],
+  },
+  {
+    name: "hamstring stretch",
+    type: "stretch",
+    aliases: ["hamstring mobility"],
+  },
+  {
+    name: "calf stretch",
+    type: "stretch",
+    aliases: ["calf mobility"],
+  },
+  {
+    name: "thoracic rotation",
+    type: "stretch",
+    aliases: ["t spine rotation", "thoracic mobility"],
+  },
+  {
+    name: "ankle mobility",
+    type: "stretch",
+    aliases: ["ankle stretch", "ankle dorsiflexion"],
+  },
+];
